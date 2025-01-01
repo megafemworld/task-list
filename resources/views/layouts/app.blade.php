ab52eb73a7e16718da1db7e50f8e69662b53963e
megafemworld <144464678+megafemworld@users.noreply.github.com>
2025-01-01 20:59:07 +0100

    task lara-git-commit-editmsg

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel task list App</title>
+    </head>
+
+    <body>
+        <h1>@yield('title')</h1>
+        <div>
+            @yield('content')
+        </div>
+    </body>
