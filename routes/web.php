3cb268f9088c2d438bd81d8dce7b5cb6b3bfb2d8
megafemworld <144464678+megafemworld@users.noreply.github.com>
2025-01-20 16:15:07 +0100

    error show

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 
 Route::get('/', function()
@@ -11,7 +12,7 @@ Route::get('/', function()
 });
 
 Route::get('/tasks', function ()  {
-    return view('index', ['tasks' => \App\Models\Task::latest()->where('completed', true)->get()
+    return view('index', ['tasks' => Task::latest()->where('completed', true)->get()
   ]);
 })->name('tasks.index');
 
@@ -19,11 +20,24 @@ Route::view('/tasks/create', 'create')
     ->name('tasks.create');
 
 Route::get('/tasks/{id}', function ($id){
-    return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
+    return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
 
 Route::post('/tasks', function (Request $request) {
-    dd($request->all());
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id]);
 })->name('tasks.store');
 
 // Route::get('/hello', function () {
